756483cccc7efad71eeb10c90099a22abd369738
Bryan Nystrom <bnystrom@gmail.com>
2022-11-09 16:09:02 -0600

    Add comment with packer github install

--- lua/javadog/packer.lua
+++ lua/javadog/packer.lua
@@ -1,4 +1,7 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
+--
+-- You need to install packer on your system via github first
+-- git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
