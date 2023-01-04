5154eebc089ccd4ae7e3a3d88e6046b8940329b4
Bryan Nystrom <bnystrom@anl.gov>
2023-01-04 10:51:54 -0600

    Add plugin: telescope

--- lua/javadog/packer.lua
+++ lua/javadog/packer.lua
@@ -10,4 +10,8 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'folke/tokyonight.nvim'
+  use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.0',
+      requires = { {'nvim-lua/plenary.nvim'} }
+  }
 end)
